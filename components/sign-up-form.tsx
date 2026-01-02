dab64048af5b1ff1150ab6435899117148ce23c2
Abigail Briones <abigailbrionesaranda@gmail.com>
2026-01-02 12:43:48 -0500

    sign up form

--- components/sign-up-form.tsx
+++ components/sign-up-form.tsx
@@ -107,17 +107,17 @@ export function SignUpForm({
                 {isLoading ? "Creating an account..." : "Sign up"}
               </Button>
               <div className="relative my-4">
-  <div className="absolute inset-0 flex items-center">
-    <span className="w-full border-t" />
-  </div>
-  <div className="relative flex justify-center text-xs uppercase">
-    <span className="bg-background px-2 text-muted-foreground">
-      Or continue with
-    </span>
-  </div>
-</div>
+                <div className="absolute inset-0 flex items-center">
+                  <span className="w-full border-t" />
+                </div>
+                <div className="relative flex justify-center text-xs uppercase">
+                  <span className="bg-background px-2 text-muted-foreground">
+                    Or continue with
+                  </span>
+                </div>
+              </div>
 
-<GoogleButton />
+              <GoogleButton />
             </div>
             <div className="mt-4 text-center text-sm">
               Already have an account?{" "}
