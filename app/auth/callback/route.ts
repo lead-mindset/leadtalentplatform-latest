202779fee7f40646e3bafe0fec91f8ab4d0e995a
Abigail Briones <abigailbrionesaranda@gmail.com>
2026-01-10 15:54:47 -0500

    use supabase object

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from 'next/server'
-import { createClient } from '@/lib/supabase/server'
+import { supabase } from '@/lib/supabase/client'
 
 export async function GET(request: Request) {
   const { searchParams, origin } = new URL(request.url)
@@ -10,7 +10,6 @@ export async function GET(request: Request) {
   }
 
   if (code) {
-    const supabase = await createClient()
     const { error } = await supabase.auth.exchangeCodeForSession(code)
     if (!error) {
       const forwardedHost = request.headers.get('x-forwarded-host')
