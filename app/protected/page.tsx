51425d09387f54674d240c2cb3f79a55500df6fd
Abigail Briones <abigailbrionesaranda@gmail.com>
2026-01-18 14:57:56 -0500

    use type

--- app/protected/page.tsx
+++ app/protected/page.tsx
@@ -2,6 +2,7 @@ import { createClient } from "@/lib/supabase/server";
 import { Suspense } from "react";
 import { redirect } from "next/navigation";
 import ProfileUpdateForm from "./profile-update-form";
+import type { ProfileData } from "@/lib/memberschema";
 
 async function ProfileData() {
   const supabase = await createClient();
@@ -31,18 +32,16 @@ async function ProfileData() {
     console.error("Error fetching profile:", profileError);
   }
 
-  const combinedData = {
-      id: userData?.id,          
-
-    full_name: userData?.name || '',
-    phone: userData?.phone || '',
-    lead_chapter: userData?.chapterId || undefined,
-    
-    career: profileData?.major || '',
-    graduationYear: profileData?.graduationYear || undefined,
-    skills: profileData?.skills || [],
-    linkedin_url: profileData?.linkedinUrl || '',
-    consentRecruiterVisibility: profileData?.consentRecruiterVisibility || false,
+   const combinedData: ProfileData = {
+    id: userData.id,
+    full_name: userData.name || '',
+    phone: userData.phone || '',
+    lead_chapter: userData.chapterId || '',
+    career: profileData.major || '',
+    graduationYear: profileData.graduationYear || 0,
+    skills: profileData.skills || [],
+    linkedin_url: profileData.linkedinUrl || '',
+    consentRecruiterVisibility: profileData.consentRecruiterVisibility || false,
   };
 
   return <ProfileUpdateForm initialData={combinedData} />;
