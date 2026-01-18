78e6cbdd70138b9cba087918e22969db9eb78291
Abigail Briones <abigailbrionesaranda@gmail.com>
2026-01-18 16:48:50 -0500

    remove unnecesary code

--- app/onboarding/page.tsx
+++ app/onboarding/page.tsx
@@ -1,17 +1,4 @@
-import { redirect } from "next/navigation";
-import { createClient } from "@/lib/supabase/server";
 import Onboarding from "@/components/onboarding";
-
-async function UserDetails() {
-  const supabase = await createClient();
-  const { data, error } = await supabase.auth.getClaims();
-
-  if (error || !data?.claims) {
-    redirect("/auth/login");
-  }
-
-  return JSON.stringify(data.claims, null, 2);
-}
 
 export default function ProtectedPage() {
   return (
