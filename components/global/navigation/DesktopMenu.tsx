247a54940b807f20079d37a19c004114817612bb
Abigail Briones <abigailbrionesaranda@gmail.com>
2026-02-06 10:13:49 -0500

    update interface

--- components/global/navigation/DesktopMenu.tsx
+++ components/global/navigation/DesktopMenu.tsx
@@ -1,24 +1,25 @@
 "use client";
 
-import Link from "next/link";
-import { NavLink } from "@/lib/types";
+import { Link } from '@/i18n/routing';
 import AuthButtons from "./auth-buttons";
+import type { MenuItem } from './MobMenu';
 
 interface DesktopNavProps {
   user: any | null;
-  links: NavLink[];
+  items: MenuItem[]; 
 }
 
-export default function DesktopNav({ user, links }: DesktopNavProps) {
+export default function DesktopNav({ user, items }: DesktopNavProps) {
   return (
     <>
       <ul className="hidden lg:flex gap-6 ml-10">
-        {links.map((link) => (
-          <li key={link.href}>
-            <Link href={link.href}>{link.label}</Link>
+        {items.map((item) => (
+          <li key={item.href}>
+            <Link href={item.href}>{item.name}</Link>
           </li>
         ))}
       </ul>
+
       <div className="ml-auto hidden lg:flex gap-2">
         <AuthButtons user={user} />
       </div>
