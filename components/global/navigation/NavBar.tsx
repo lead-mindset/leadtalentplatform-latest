27164a9daa6ebd65fc07100ac31631dbc1f29812
Abigail Briones <abigailbrionesaranda@gmail.com>
2026-03-01 16:24:37 -0500

    simplify navbar

--- components/global/navigation/NavBar.tsx
+++ components/global/navigation/NavBar.tsx
@@ -22,7 +22,7 @@ export default function NavBar({ user, menuItems, memberId, signInLabel }: NavBa
     <nav className="flex items-center h-16 px-6 border-b bg-background relative z-50">
       <Link href="/" className="flex items-center font-bold gap-2 shrink-0">
         <img src="/leadl2.svg" alt="LEAD" width={32} height={32} />
-        LEAD Talent Platform
+        <span className='max-sm:hidden'>LEAD Talent Platform</span>
       </Link>
 
       <div className="flex gap-4 items-center flex-1 justify-between">
@@ -30,24 +30,6 @@ export default function NavBar({ user, menuItems, memberId, signInLabel }: NavBa
           <DesktopNav user={user} items={menuItems} />
 
           <div className="lg:hidden ml-auto flex items-center gap-2">
-            {user ? (
-              memberId ? (
-                <Badge variant="secondary" className="text-xs font-mono">
-                  #{memberId}
-                </Badge>
-              ) : (
-                <Avatar className="h-8 w-8 border border-border">
-                  <AvatarImage src={user.avatar} alt={user.name} />
-                  <AvatarFallback className="text-xs font-semibold">
-                    {getInitials(user.name)}
-                  </AvatarFallback>
-                </Avatar>
-              )
-            ) : (
-              <Button asChild size="sm" variant="outline">
-                <Link href="/auth/login">{signInLabel}</Link>
-              </Button>
-            )}
             <MobileNav menuItems={menuItems} user={user} memberId={memberId} />
           </div>
         </div>
