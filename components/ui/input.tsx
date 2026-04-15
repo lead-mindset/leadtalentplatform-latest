48159ef62336548e6e7da20156c022f9156fcab2
Abigail Briones <abigailbrionesaranda@gmail.com>
2026-04-15 12:24:34 -0400

    ui changes

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -8,8 +8,6 @@ function Input({ className, type, ...props }: React.ComponentProps<"input">) {
       data-slot="input"
       className={cn(
         "h-9 w-full min-w-0 rounded-md border border-input bg-transparent px-3 py-1 text-base shadow-xs transition-colors transition-shadow outline-none selection:bg-primary selection:text-primary-foreground file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm dark:bg-input/30",
-        // Use a lighter border on focus and remove any visible ring
-        "focus:border-border focus:ring-3 focus:ring/50",
         // Keep invalid states
         "aria-invalid:border-destructive aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40",
         className
