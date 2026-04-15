f5dfed24ec11671163c27ab42701bbaaaa4812d1
Abigail Briones <abigailbrionesaranda@gmail.com>
2026-04-15 14:33:48 -0400

    index ts

---
+++ components/ui/index.ts
@@ -0,0 +1,39 @@
+export { Alert, AlertTitle, AlertDescription } from './alert';
+export {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from './alert-dialog';
+export { Avatar, AvatarFallback, AvatarImage } from './avatar';
+export { Badge, badgeVariants } from './badge';
+export { Button } from './button';
+export { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from './card';
+export { Checkbox } from './checkbox';
+export { Command } from './command';
+export { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from './dialog';
+export {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from './dropdown-menu';
+export { Input } from './input';
+export { Label } from './label';
+export { Popover, PopoverContent, PopoverTrigger } from './popover';
+export { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from './select';
+export { Separator } from './separator';
+export { Skeleton } from './skeleton';
+export { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from './table';
+export { Tabs, TabsContent, TabsList, TabsTrigger } from './tabs';
+export { Textarea } from './textarea';
+export { Toggle } from './toggle';
+export { ToggleGroup, ToggleGroupItem } from './toggle-group';
+export { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from './tooltip';
