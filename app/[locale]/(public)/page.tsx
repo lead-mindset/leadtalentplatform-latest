0dcc163b4561e9d60bbf798e7d94dab87ea2f51c
Abigail Briones <abigailbrionesaranda@gmail.com>
2026-04-19 16:54:49 -0400

    use allies

--- app/[locale]/(public)/page.tsx
+++ app/[locale]/(public)/page.tsx
@@ -1,6 +1,6 @@
 import { Navbar } from "./_components/navbar";
 import { Hero } from "./_components/hero";
-import { ProofStrip } from "./_components/proofstrip";
+import { Allies } from "./_components/allies";
 import { ValueCards } from "./_components/value-cards";
 import { CompanyBand } from "./_components/company-band";
 import { FinalCTA } from "./_components/final-cta";
@@ -16,12 +16,12 @@ export const metadata = {
 
 export default function MarketingHomePage() {
   return (
-    <div className="min-h-screen flex flex-col text-foreground antialiased relative overflow-hidden bg-background">
+    <div className="min-h-screen flex flex-col text-foreground antialiased relative overflow-hidden">
       <div className="relative z-10">
         <Navbar />
         <main className="flex-1">
           <Hero />
-          <ProofStrip />
+          <Allies />
           <ValueCards />
           <CompanyBand />
           <FinalCTA />
